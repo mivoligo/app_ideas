0717c7978aced651d570486c7a13fd351ec4843c
Michal Predotka <mivoligo@gmail.com>
2022-01-11 14:04:39 +0000

    Change UI of idea card

--- lib/ideas/view/idea_card.dart
+++ lib/ideas/view/idea_card.dart
@@ -5,18 +5,59 @@ class IdeaCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return SizedBox(
-      height: 150,
-      child: Card(
-        color: Colors.green,
-        child: Column(
-          children: const [
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8.0),
+          gradient: const LinearGradient(
+            colors: [
+              Color(0xFF222935),
+              Color(0xFF0F151C),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomCenter,
+          ),
+          boxShadow: const [
+            BoxShadow(
+              color: Color(0xFF434B65),
+              offset: Offset(0, -1),
+            )
+          ],
+        ),
+        child: Row(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            const SizedBox(width: 120, height: 120, child: Placeholder()),
             Expanded(
-              child: Placeholder(),
+              child: Padding(
+                padding: const EdgeInsets.all(8.0),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  mainAxisSize: MainAxisSize.min,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 8.0),
+                      child: Text(
+                        'Name of the app idea',
+                        style: Theme.of(context)
+                            .textTheme
+                            .headline5
+                            ?.copyWith(color: Colors.grey.shade300),
+                      ),
+                    ),
+                    Text(
+                      'some text which is longer much maybe even a bit longer',
+                      style: Theme.of(context)
+                          .textTheme
+                          .titleMedium
+                          ?.copyWith(color: Colors.grey.shade400),
+                    ),
+                  ],
+                ),
+              ),
             ),
-            Text('some text'),
-            Text('some text which is longer much'),
-            Text('some text'),
           ],
         ),
       ),
