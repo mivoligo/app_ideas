71ed097296eb441859064c2c7a39dbb1c13d7f9c
Michal Predotka <mivoligo@gmail.com>
2022-01-12 21:47:02 +0000

    Organize home page UI

--- lib/home/view/home_page.dart
+++ lib/home/view/home_page.dart
@@ -9,41 +9,81 @@ class HomePage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       body: Container(
-        color: Colors.lightBlue,
-        child: Column(
-          children: [
-            const Expanded(flex: 2, child: SizedBox()),
-            Expanded(
-              child: ClickableCard(
-                title: 'Browse Ideas',
-                onTap: () => Navigator.of(context).push(
-                  MaterialPageRoute(
-                    builder: (context) => const IdeasPage(),
+        decoration: const BoxDecoration(
+          gradient: LinearGradient(
+            colors: [
+              Color(0xFF1B2234),
+              Color(0xFF131823),
+            ],
+            begin: Alignment.topCenter,
+            end: Alignment.bottomRight,
+          ),
+        ),
+        child: Padding(
+          padding: const EdgeInsets.all(12.0),
+          child: Column(
+            children: [
+              Expanded(
+                flex: 2,
+                child: Center(
+                  child: Text(
+                    'App Ideas',
+                    style: Theme.of(context).textTheme.displayLarge?.copyWith(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                        ),
                   ),
                 ),
               ),
-            ),
-            Expanded(
-              child: Row(
-                children: [
-                  Expanded(
-                    flex: 4,
-                    child: ClickableCard(
-                      title: 'Random Idea',
-                      onTap: () {},
+              Expanded(
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 4,
+                      child: ClickableCard(
+                        title: 'Propose your idea',
+                        onTap: () {},
+                      ),
                     ),
-                  ),
-                  Expanded(
-                    flex: 3,
-                    child: ClickableCard(
-                      title: 'Propose Your Idea',
-                      onTap: () {},
+                    const SizedBox(width: 12),
+                    Expanded(
+                      flex: 3,
+                      child: ClickableCard(
+                        title: 'About this app',
+                        onTap: () {},
+                      ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
-            ),
-          ],
+              const SizedBox(height: 12),
+              Expanded(
+                child: Row(
+                  children: [
+                    Expanded(
+                      flex: 1,
+                      child: ClickableCard(
+                        title: 'Random',
+                        onTap: () {},
+                      ),
+                    ),
+                    const SizedBox(width: 12),
+                    Expanded(
+                      flex: 2,
+                      child: ClickableCard(
+                        title: 'Browse Ideas',
+                        onTap: () => Navigator.of(context).push(
+                          MaterialPageRoute(
+                            builder: (context) => const IdeasPage(),
+                          ),
+                        ),
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
         ),
       ),
     );
