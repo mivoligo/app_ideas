f24817baecc1385ba470fa9b1ca8339993839aee
Michal Predotka <mivoligo@gmail.com>
2022-01-14 22:42:47 +0000

    Add page for new idea proposal

---
+++ lib/new_idea/new_idea_page.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class NewIdeaPage extends StatelessWidget {
+  const NewIdeaPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Text('new idea'),
+    );
+  }
+}
